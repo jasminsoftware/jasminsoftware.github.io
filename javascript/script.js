7f2e4fb45484f3fe4720a33ff49bd476cf4ee6fe
Diogo Rocha <diogo.rocha@primaverabss.com>
2017-05-24 14:49:25 +0100

    Add Documentation pages

---
+++ javascript/script.js
@@ -0,0 +1,125 @@
+"use strict";
+
+function toggleVisibility(element, toToggleElementId) {
+    var toToggleElement = $('#' + toToggleElementId);
+    
+    if(toToggleElement === undefined) {
+        return;
+    }
+
+    var displayValue = toToggleElement.css("display");
+
+    if( displayValue === 'block') {
+        toToggleElement.css('display', 'none');
+        element.children[0].className = 'fa fa-chevron-down';
+    } else {
+        toToggleElement.css('display', 'block');
+        element.children[0].className = 'fa fa-chevron-up';
+    }
+}
+
+(function(){
+
+    var menuVisible = false;
+    var searchIndexList = window.searchIndexList;
+    var substringMatcher = null;
+    var search = null;
+
+    function init(){
+        $(document).ready(function() {
+            $("#menu-button").click(function(){
+                showHideMenu();
+            });
+
+            $(window).resize(function() {
+                hideMenu();
+            });
+
+            search = new Bloodhound({
+                datumTokenizer: Bloodhound.tokenizers.obj.whitespace('keywords'),
+                queryTokenizer: Bloodhound.tokenizers.whitespace,
+                local: searchIndexList
+            });
+
+            $('#search .typeahead').typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 1
+            },
+            {
+                limit: 10,
+                name: 'description',
+                display: 'description',
+                source: search.ttAdapter(),
+                templates: {
+                    empty: [].join('\n'),
+                    suggestion: Handlebars.compile('<a href="{{url}}" class="search-result"><strong>{{description}}</strong></a>')
+                }
+            });
+
+            // scroll to the sidebar menu item
+            $('#sidebar')[0].scrollTop = $('#current-menu-item')[0].offsetTop;
+        });
+    }
+
+    function showHideMenu(){
+        if(menuVisible){
+            hideMenu()
+        } else {
+            showMenu()
+        }
+    }
+
+    function showMenu(){
+        menuVisible = true;
+        $("#sidebar").addClass("sidebar-show");
+    }
+
+    function hideMenu() {
+        menuVisible = false;
+        $("#sidebar").removeClass("sidebar-show");
+    }
+
+    function highlight(name) {
+        alert(name);
+    }
+
+    function toggleVisibility(elementId) {
+        var element = $('#' + elementId);
+        
+        if(element === undefined) {
+            return;
+        }
+
+        var displayValue = element.css("display");
+
+        if( displayValue === 'none') {
+
+        }
+    }
+
+    substringMatcher = function(items) {
+        return function findMatches(q, cb) {
+            var matches, substringRegex;
+
+            // an array that will be populated with substring matches
+            matches = [];
+
+            // regex used to determine if a string contains the substring `q`
+            substrRegex = new RegExp(q, 'i');
+
+            // iterate through the pool of strings and for any string that
+            // contains the substring `q`, add it to the `matches` array
+            $.each(items, function(i, item) {
+                if (substrRegex.test(item.keywords)) {
+                    matches.push(item.description);
+                }
+            });
+
+            cb(matches);
+        };
+    };
+
+    init();
+
+})();
---
+++ javascript/searchindex.js
@@ -0,0 +1,528 @@
+﻿window.searchIndexList = [
+	{
+        url: 'corepatterns.cultures.html',
+        description: "Platform | Culture",
+        keywords : "Culture"
+	},
+	{
+        url: 'corepatterns.addresses.html',
+        description: "Platform | Address",
+        keywords : "Address"
+	},
+	{
+        url: 'corepatterns.countries.html',
+        description: "Platform | Country",
+        keywords : "Country"
+	},
+	{
+        url: 'corepatterns.postalzones.html',
+        description: "Platform | Postal Code",
+        keywords : "Postal Code"
+	},
+	{
+        url: 'corepatterns.regions.html',
+        description: "Platform | Region",
+        keywords : "Region"
+	},
+	{
+        url: 'corepatterns.currencies.html',
+        description: "Platform | Currency",
+        keywords : "Currency"
+	},
+	{
+        url: 'corepatterns.companies.html',
+        description: "Platform | Company",
+        keywords : "Company"
+	},
+	{
+        url: 'corepatterns.units.html',
+        description: "Platform | Unit",
+        keywords : "Unit"
+	},
+	{
+        url: 'corepatterns.unitdimensions.html',
+        description: "Platform | Unit Dimension",
+        keywords : "Unit Dimension"
+	},
+	{
+        url: 'corepatterns.powerbiinfos.html',
+        description: "Platform | Power Bi Info",
+        keywords : "Power Bi Info"
+	},
+	{
+        url: 'corepatterns.contacts.html',
+        description: "Platform | Contact",
+        keywords : "Contact"
+	},
+	{
+        url: 'corepatterns.contacttypes.html',
+        description: "Platform | Contact Type",
+        keywords : "Contact Type"
+	},
+	{
+        url: 'corepatterns.contactgroups.html',
+        description: "Platform | Contact Group",
+        keywords : "Contact Group"
+	},
+	{
+        url: 'corepatterns.fiscaltimezones.html',
+        description: "Platform | Fiscal Time Zone",
+        keywords : "Fiscal Time Zone"
+	},
+	{
+        url: 'operations.notifications.html',
+        description: "Platform | Notification",
+        keywords : "Notification"
+	},
+	{
+        url: 'operations.emailtemplates.html',
+        description: "Platform | Email Template",
+        keywords : "Email Template"
+	},
+	{
+        url: 'businesscore.series.html',
+        description: "Master Data Business Entities | Serie",
+        keywords : "Serie"
+	},
+	{
+        url: 'businesscore.exchangeratesdocuments.html',
+        description: "Master Data Business Entities | Exchange Rates Document",
+        keywords : "Exchange Rates Document"
+	},
+	{
+        url: 'businesscore.parties.html',
+        description: "Master Data Business Entities | Entity",
+        keywords : "Entity"
+	},
+	{
+        url: 'businesscore.items.html',
+        description: "Master Data Business Entities | Item",
+        keywords : "Item"
+	},
+	{
+        url: 'businesscore.assortments.html',
+        description: "Master Data Business Entities | Assortment",
+        keywords : "Assortment"
+	},
+	{
+        url: 'businesscore.brands.html',
+        description: "Master Data Business Entities | Brand",
+        keywords : "Brand"
+	},
+	{
+        url: 'businesscore.brandmodels.html',
+        description: "Master Data Business Entities | Model",
+        keywords : "Model"
+	},
+	{
+        url: 'taxescore.fiscaldocumenttypes.html',
+        description: "Taxes | Fiscal Document Type",
+        keywords : "Fiscal Document Type"
+	},
+	{
+        url: 'taxescore.taxtypecodes.html',
+        description: "Taxes | Tax Type Code",
+        keywords : "Tax Type Code"
+	},
+	{
+        url: 'taxescore.taxclasses.html',
+        description: "Taxes | Tax Class",
+        keywords : "Tax Class"
+	},
+	{
+        url: 'taxescore.taxsubclasses.html',
+        description: "Taxes | Tax Sub Class",
+        keywords : "Tax Sub Class"
+	},
+	{
+        url: 'taxescore.taxregionalareas.html',
+        description: "Taxes | Tax Regional Area",
+        keywords : "Tax Regional Area"
+	},
+	{
+        url: 'taxescore.partytaxschemas.html',
+        description: "Taxes | Party Tax Schema",
+        keywords : "Party Tax Schema"
+	},
+	{
+        url: 'taxescore.exemptionreasoncodes.html',
+        description: "Taxes | Exemption Reason Code",
+        keywords : "Exemption Reason Code"
+	},
+	{
+        url: 'taxescore.itemtaxschemas.html',
+        description: "Taxes | Item Tax Schema",
+        keywords : "Item Tax Schema"
+	},
+	{
+        url: 'taxescore.fiscalclassifications.html',
+        description: "Taxes | Tax Classification",
+        keywords : "Tax Classification"
+	},
+	{
+        url: 'taxescore.economicactivityclassifications.html',
+        description: "Taxes | Economic Activity Classification",
+        keywords : "Economic Activity Classification"
+	},
+	{
+        url: 'taxescore.withholdingtaxcodes.html',
+        description: "Taxes | WHT Code",
+        keywords : "WHT Code"
+	},
+	{
+        url: 'taxescore.partywithholdingtaxschemas.html',
+        description: "Taxes | Party WHT Schema",
+        keywords : "Party WHT Schema"
+	},
+	{
+        url: 'taxescore.itemwithholdingtaxschemas.html',
+        description: "Taxes | Item WHT Schema",
+        keywords : "Item WHT Schema"
+	},
+	{
+        url: 'taxescore.paymentmethodfiscalclassifications.html',
+        description: "Taxes | Payment Method Fiscal Class",
+        keywords : "Payment Method Fiscal Class"
+	},
+	{
+        url: 'taxescore.fiscalwitholdingtaxtypes.html',
+        description: "Taxes | Fiscal Witholding Tax Type",
+        keywords : "Fiscal Witholding Tax Type"
+	},
+	{
+        url: 'taxescore.withholdingtaxtypes.html',
+        description: "Taxes | WHT Type",
+        keywords : "WHT Type"
+	},
+	{
+        url: 'taxescore.authoritycommunicationrequests.html',
+        description: "Taxes | AT Communication Request",
+        keywords : "AT Communication Request"
+	},
+	{
+        url: 'financialcore.paymentmethods.html',
+        description: "Financials | Payment Method",
+        keywords : "Payment Method"
+	},
+	{
+        url: 'financialcore.paymentterms.html',
+        description: "Financials | Payment Term",
+        keywords : "Payment Term"
+	},
+	{
+        url: 'financialcore.paymenttypes.html',
+        description: "Financials | Payment Type",
+        keywords : "Payment Type"
+	},
+	{
+        url: 'financialcore.receipttypes.html',
+        description: "Financials | Receipt Type",
+        keywords : "Receipt Type"
+	},
+	{
+        url: 'financialcore.financialsetups.html',
+        description: "Financials | Financial Setup",
+        keywords : "Financial Setup"
+	},
+	{
+        url: 'financialcore.pettycashes.html',
+        description: "Financials | Petty Cash",
+        keywords : "Petty Cash"
+	},
+	{
+        url: 'financialcore.financialaccounts.html',
+        description: "Financials | Financial Account",
+        keywords : "Financial Account"
+	},
+	{
+        url: 'financialcore.cashflowitems.html',
+        description: "Financials | Cash Flow Item",
+        keywords : "Cash Flow Item"
+	},
+	{
+        url: 'financialcore.financialinstitutions.html',
+        description: "Financials | Financial Institution",
+        keywords : "Financial Institution"
+	},
+	{
+        url: 'materialscore.materialssetups.html',
+        description: "Materials Management | Materials Setup",
+        keywords : "Materials Setup"
+	},
+	{
+        url: 'materialscore.itemsubtypes.html',
+        description: "Materials Management | Item Subtype",
+        keywords : "Item Subtype"
+	},
+	{
+        url: 'materialscore.warehouses.html',
+        description: "Materials Management | Warehouse",
+        keywords : "Warehouse"
+	},
+	{
+        url: 'materialscore.materialsitems.html',
+        description: "Materials Management | Materials Item",
+        keywords : "Materials Item"
+	},
+	{
+        url: 'salescore.salessetups.html',
+        description: "Sales | Sales Setup",
+        keywords : "Sales Setup"
+	},
+	{
+        url: 'salescore.salespersons.html',
+        description: "Sales | Salesperson",
+        keywords : "Salesperson"
+	},
+	{
+        url: 'salescore.pricelists.html',
+        description: "Sales | Price List",
+        keywords : "Price List"
+	},
+	{
+        url: 'salescore.salesitems.html',
+        description: "Sales | Sales Item",
+        keywords : "Sales Item"
+	},
+	{
+        url: 'salescore.customergroups.html',
+        description: "Sales | Customer Group",
+        keywords : "Customer Group"
+	},
+	{
+        url: 'salescore.customerparties.html',
+        description: "Sales | Customer Party",
+        keywords : "Customer Party"
+	},
+	{
+        url: 'salescore.saleschannels.html',
+        description: "Sales | Sales Channel",
+        keywords : "Sales Channel"
+	},
+	{
+        url: 'salescore.ordertypes.html',
+        description: "Sales | Order Type",
+        keywords : "Order Type"
+	},
+	{
+        url: 'salescore.quotationtypes.html',
+        description: "Sales | Quotation Type",
+        keywords : "Quotation Type"
+	},
+	{
+        url: 'salescore.invoicetypes.html',
+        description: "Sales | Invoice Type",
+        keywords : "Invoice Type"
+	},
+	{
+        url: 'salescore.memotypes.html',
+        description: "Sales | Memo Type",
+        keywords : "Memo Type"
+	},
+	{
+        url: 'logisticscore.deliveryterms.html',
+        description: "Logistics | Delivery Term",
+        keywords : "Delivery Term"
+	},
+	{
+        url: 'logisticscore.memoreasons.html',
+        description: "Logistics | Memo Reason",
+        keywords : "Memo Reason"
+	},
+	{
+        url: 'logisticscore.goodsreceiptnotetypes.html',
+        description: "Logistics | Goods Receipt Type",
+        keywords : "Goods Receipt Type"
+	},
+	{
+        url: 'logisticscore.deliverytypes.html',
+        description: "Logistics | Delivery Type",
+        keywords : "Delivery Type"
+	},
+	{
+        url: 'logisticscore.shippingmethods.html',
+        description: "Logistics | Shipping Method",
+        keywords : "Shipping Method"
+	},
+	{
+        url: 'sales.orders.html',
+        description: "Sales | Order",
+        keywords : "Order"
+	},
+	{
+        url: 'sales.quotations.html',
+        description: "Sales | Quotation",
+        keywords : "Quotation"
+	},
+	{
+        url: 'purchasescore.suppliergroups.html',
+        description: "Purchases | Supplier Group",
+        keywords : "Supplier Group"
+	},
+	{
+        url: 'purchasescore.supplierparties.html',
+        description: "Purchases | Supplier",
+        keywords : "Supplier"
+	},
+	{
+        url: 'purchasescore.purchasesitems.html',
+        description: "Purchases | Purchase Item",
+        keywords : "Purchase Item"
+	},
+	{
+        url: 'purchasescore.ordertypes.html',
+        description: "Purchases | Order Type",
+        keywords : "Order Type"
+	},
+	{
+        url: 'purchasescore.invoicetypes.html',
+        description: "Purchases | Invoice Type",
+        keywords : "Invoice Type"
+	},
+	{
+        url: 'purchasescore.memotypes.html',
+        description: "Purchases | Memo Type",
+        keywords : "Memo Type"
+	},
+	{
+        url: 'purchasescore.purchasessetups.html',
+        description: "Purchases | Purchases Setup",
+        keywords : "Purchases Setup"
+	},
+	{
+        url: 'purchases.orders.html',
+        description: "Purchases | Order",
+        keywords : "Order"
+	},
+	{
+        url: 'purchases.summaryvalues.html',
+        description: "Purchases | Summary Value",
+        keywords : "Summary Value"
+	},
+	{
+        url: 'shipping.deliveries.html',
+        description: "Shipping | Delivery",
+        keywords : "Delivery"
+	},
+	{
+        url: 'shipping.shippingrequests.html',
+        description: "Shipping | Shipping Request",
+        keywords : "Shipping Request"
+	},
+	{
+        url: 'shipping.processorders.html',
+        description: "Shipping | Process Order",
+        keywords : "Process Order"
+	},
+	{
+        url: 'billing.invoices.html',
+        description: "Sales | Invoice",
+        keywords : "Invoice"
+	},
+	{
+        url: 'billing.memos.html',
+        description: "Sales | Memo",
+        keywords : "Memo"
+	},
+	{
+        url: 'billing.processorders.html',
+        description: "Sales | Process Order",
+        keywords : "Process Order"
+	},
+	{
+        url: 'billing.processreturnorders.html',
+        description: "Sales | Process Return Order",
+        keywords : "Process Return Order"
+	},
+	{
+        url: 'billing.companyinvoices.html',
+        description: "Sales | Company Invoice",
+        keywords : "Company Invoice"
+	},
+	{
+        url: 'goodsreceipt.processorders.html',
+        description: "Goods Receipt | Process Order",
+        keywords : "Process Order"
+	},
+	{
+        url: 'materialsmanagement.itemadjustments.html',
+        description: "Materials Management | Item Adjustment",
+        keywords : "Item Adjustment"
+	},
+	{
+        url: 'materialsmanagement.adjustmentreasons.html',
+        description: "Materials Management | Adjustment Reason",
+        keywords : "Adjustment Reason"
+	},
+	{
+        url: 'taxes.legaldeclarations.html',
+        description: "Taxes | SAF-T (PT)",
+        keywords : "SAF-T (PT)"
+	},
+	{
+        url: 'taxes.obligations.html',
+        description: "Taxes | Obligation",
+        keywords : "Obligation"
+	},
+	{
+        url: 'invoicereceipt.invoices.html',
+        description: "Purchases | Invoice",
+        keywords : "Invoice"
+	},
+	{
+        url: 'invoicereceipt.memos.html',
+        description: "Purchases | Memo",
+        keywords : "Memo"
+	},
+	{
+        url: 'invoicereceipt.invoicereceiptnotifications.html',
+        description: "Purchases | Invoice Receipt Notification",
+        keywords : "Invoice Receipt Notification"
+	},
+	{
+        url: 'invoicereceipt.processorders.html',
+        description: "Purchases | Process Order",
+        keywords : "Process Order"
+	},
+	{
+        url: 'invoicereceipt.processreturnorders.html',
+        description: "Purchases | Process Return Order",
+        keywords : "Process Return Order"
+	},
+	{
+        url: 'accountspayable.processopenitems.html',
+        description: "Accounts Payable | Create Payment",
+        keywords : "Create Payment"
+	},
+	{
+        url: 'accountsreceivable.receipts.html',
+        description: "Accounts Receivable | Receipt",
+        keywords : "Receipt"
+	},
+	{
+        url: 'accountsreceivable.processopenitems.html',
+        description: "Accounts Receivable | Create Receipt",
+        keywords : "Create Receipt"
+	},
+	{
+        url: 'ecommerce.ecommerceitems.html',
+        description: "Online Store | ECommerce Item",
+        keywords : "ECommerce Item"
+	},
+	{
+        url: 'ecommerce.ecommercesetups.html',
+        description: "Online Store | ECommerce Setup",
+        keywords : "ECommerce Setup"
+	},
+	{
+        url: 'ecommerce.itemcategories.html',
+        description: "Online Store | Item Category",
+        keywords : "Item Category"
+	},
+	{
+        url: 'ecommerce.ecommercelistitems.html',
+        description: "Online Store | ECommerce List Items",
+        keywords : "ECommerce List Items"
+	},
+];
+
